8a4e4475e15f5724c6b7fba6738e6212a7b6b8ca
MatteoSB11 jgjg <matteosbarra07@gmail.com>
2024-11-28 09:57:03 +0000

    Punto 4-5

--- Sbarra/src/app/app-third-component/app-third-component.component.ts
+++ Sbarra/src/app/app-third-component/app-third-component.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { AppFourthComponentComponent } from '../app-fourth-component/app-fourth-component.component';
 
 @Component({
   selector: 'app-app-third-component',
-  imports: [],
+  imports: [AppFourthComponentComponent],
   templateUrl: './app-third-component.component.html',
   styleUrl: './app-third-component.component.css'
 })
--- Sbarra/src/app/app.component.ts
+++ Sbarra/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AppSecondComponentComponent } from './app-second-component/app-second-component.component';
+import { AppThirdComponentComponent } from './app-third-component/app-third-component.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AppSecondComponentComponent, AppThirdComponentComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
